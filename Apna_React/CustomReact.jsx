9cdfdff52f0b7114e81689fb4b324fbe5bf58f6d
Nabin <sharmanabin.0910@gmail.com>
2025-04-25 20:16:08 +0530

    Custom React Starter Pack

---
+++ Apna_React/CustomReact.jsx
@@ -0,0 +1,12 @@
+const reactElement = {
+    type : 'a',
+    props : {
+        href : 'https://google.com',
+        target : '_blank'
+    },
+    children: 'Click me to visit Google!'
+}//This is how every element is defined in react
+
+
+const mainContainer = document.getElementById('#root')
+
