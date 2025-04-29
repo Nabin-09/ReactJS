705eb7b471dfc14e142be1577622b979e22d4ec1
Nabin <sharmanabin.0910@gmail.com>
2025-04-29 13:12:09 +0530

    Initialisation of variables

--- passGen/src/App.jsx
+++ passGen/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState , useCallback } from 'react'
 
 
 
 function App() {
   const [length , setLength] = useState(8)
   const [numall, setnumall] = useState(false)
-  const [charall , setCharall] = useState(false)
+  const [charall , setCharall] = useState(false) 
+  const [password , setPassword] = useState("")
+
+  const passGenerator = useCallback(() =>{
+    let pass = ""
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numall) str+= "0123456789"
+    if(charall) str+= "@_+=&*#"
+
+    
+
+  } , [length , numall , charall, setPassword])
+
   return (
     <>
       <h1 className='text-7xl text-center'>Password Generator</h1>
