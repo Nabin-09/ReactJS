4b4f47c392be10b740a783b99c2bc99c49701013
Nabin <sharmanabin.0910@gmail.com>
2025-06-27 12:05:34 +0530

    Logic of Currency converter implemented

--- currency/src/App.jsx
+++ currency/src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyinfo'
 import './App.css'
 
 function App() {
   
+  const [amount , setAmount] = useState(0);
+  const [from , setFrom] = useState('usd')
+  const [to, setTo] = useState('inr')
+  const [convertedAmount , setConvertedAmount] = useState(0)
 
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+
+  const swap = () =>{
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(setConvertedAmount)
+  }
+  const convert= () => {  
+  setConvertedAmount(amount*currencyInfo[to])
+  }
   return (
     <>
     <h1 className = "text-3xl bg-orange-500">Currency App with Nabin</h1>
--- currency/src/components/InputBox.jsx
+++ currency/src/components/InputBox.jsx
@@ -5,22 +5,23 @@ function InputBox({
     amount ,
     onAmountChange,
     onCurrencyChange,
-    currencyOption = [],
+    currencyOptions = [],
     selectCurrency = 'usd',
     amountDisable = false,
     currencyDisable = false,
     className = "",
 }) {
-   
+    const amountId = useId()
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountId} 
+                className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id = {amountId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -35,13 +36,18 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    value={selectCurrency}
+                    onChange={(e)=>{
+                        onCurrencyChange && onCurrencyChange(e.target.value)
+                    }}
+                    disabled = {currencyDisable}
                 >
                     
-                        <option value="usd">
-                            usd
+                       {currencyOptions.map((currency) => (
+                         <option key={currency} value={currency}>
+                            {currency}
                         </option>
-                
+                       ))}
                 </select>
             </div>
         </div>
