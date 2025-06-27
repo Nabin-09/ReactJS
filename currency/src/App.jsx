556db4f8e30503ee5a825e384e7fdd2c34681a2b
Nabin <sharmanabin.0910@gmail.com>
2025-06-27 10:44:29 +0530

    useCurrencyInfo

--- currency/src/App.jsx
+++ currency/src/App.jsx
@@ -4,11 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    
+    <h1 className = "text-3xl bg-orange-500">Currency App with Nabin</h1>
     </>
   )
 }
