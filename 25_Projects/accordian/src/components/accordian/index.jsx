71a348cff5e9eb2834947d6a02408f302c55d62d
Nabin <sharmanabin.0910@gmail.com>
2025-06-28 15:33:41 +0530

    Structure of the accordian

--- 25_Projects/accordian/src/components/accordian/index.jsx
+++ 25_Projects/accordian/src/components/accordian/index.jsx
@@ -1,9 +1,33 @@
+import data from "./data";
+import { useState } from "react";
+
 
 
 export default function Accordian (){
-    return(
-        <>
-        Accordian
-        </>
-    )
+    function handleSingleSelection(getCurrentId){
+    console.log(getCurrentId);
+    setSelected(getCurrentId)
+}
+const [selected , setSelected] = useState(null);
+     return <div className="wrapper">
+        <div className="accordian">
+            {
+                data && data.length > 0 ?
+                data.map( dataItem => <div className="item">
+                    <div  onClick= {()=>handleSingleSelection(dataItem.id)} className="title">
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                    {
+                        selected === dataItem.id ?
+                        <div className="content">
+                            {dataItem.answer}
+                        </div>
+                        : null
+                    }
+                </div>) 
+                : <div>No Data found !</div>
+            }
+        </div>
+    </div>
 }
