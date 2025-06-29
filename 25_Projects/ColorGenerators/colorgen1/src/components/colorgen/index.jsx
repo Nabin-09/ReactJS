6c0571e2ad7bcd39d6d07d75a131d9024c2f0d24
Nabin <sharmanabin.0910@gmail.com>
2025-06-29 10:33:32 +0530

    Onclick Event Handlers to buttons

--- 25_Projects/ColorGenerators/colorgen1/src/components/colorgen/index.jsx
+++ 25_Projects/ColorGenerators/colorgen1/src/components/colorgen/index.jsx
@@ -10,6 +10,8 @@ export default function RandomColor(){
     const [typeOfColor , setTypeOfColor] = useState('hex');
     const [color , setColor] = useState('#000000')
 
+    
+
     return (
     <div  style = {{
         width : '100vw',
@@ -20,9 +22,9 @@ export default function RandomColor(){
         
     }} className="container">
     
-        <button>Create HEX color</button>
-        <button>Create RGB color</button>
-        <button>Create Random color</button>
+        <button onClick={()=> setTypeOfColor('hex')}>Create HEX color</button>
+        <button onClick={()=>setTypeOfColor('rgb')}>Create RGB color</button>
+        <button onClick={handleCreateRandomColor}>Create Random color</button>
 
 
     </div>
