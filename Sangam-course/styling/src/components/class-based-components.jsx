75d3b7db1a281914c9fb65bd801b5359e8325c7e
Nabin <sharmanabin.0910@gmail.com>
2025-07-10 11:09:34 +0530

    class based components

---
+++ Sangam-course/styling/src/components/class-based-components.jsx
@@ -0,0 +1,13 @@
+// outdated way to making components not working 
+
+import { Component } from "react";
+
+class ClassBasedComponent extends Component{
+    render(){
+        return <div>
+            <h3>Class based components</h3>
+        </div>
+    }
+}
+
+export default ClassBasedComponent
