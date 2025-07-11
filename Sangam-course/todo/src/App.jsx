c4314197e2696d78df0271aaccd8ddb161f31c12
Nabin <sharmanabin.0910@gmail.com>
2025-07-11 12:07:03 +0530

    3:08:00 Giving class to title - CSS styling

--- Sangam-course/todo/src/App.jsx
+++ Sangam-course/todo/src/App.jsx
@@ -1,5 +1,5 @@
 import {useState , useEffect } from "react";
-
+import classes from './styles.module.css'
 
 
 
@@ -17,7 +17,15 @@ function App() {
 
       console.log(result)
 
-      
+      if(result?.todos && result?.todos?.length > 0 ){
+        setTodoList(result?.todos);
+        setLoading(false);
+        setErrorMsg('');
+      }else{
+        setTodoList([]);
+        setLoading(false);
+        setErrorMsg('')
+      }
 
     }catch(e){
       console.log(e);
@@ -30,9 +38,9 @@ function App() {
   },[])
 
   return (
-    <>
-    <h1>Simple ToDo using material UI</h1>
-    </>
+    <div>
+    <h1 className={classes.headerTitle}>Simple ToDo using material UI</h1>
+    </div>
   )
 }
 
