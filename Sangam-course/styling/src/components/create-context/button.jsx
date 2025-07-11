9e19ab68325c4d1cd3ddb1eddf921079f6d99433
Nabin <sharmanabin.0910@gmail.com>
2025-07-11 11:37:20 +0530

    Using Global context

--- Sangam-course/styling/src/components/create-context/button.jsx
+++ Sangam-course/styling/src/components/create-context/button.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
 
 
 
 function ContextButtonComponent(){
-    return <button>Change Theme</button>
+
+    const {setTheme , theme} = useContext(GlobalContext) 
+    return <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Change Theme</button>
 }
 
 export default ContextButtonComponent;
--- Sangam-course/styling/src/components/create-context/text.jsx
+++ Sangam-course/styling/src/components/create-context/text.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
 
 
 
 function ContextTextComponent(){
-    return <h1>Nabin Sharma</h1>
+    const {theme} = useContext(GlobalContext);
+
+
+    return <h1 style={{fontSize : theme === 'light' ? '50px' : '100px' , backgroundColor : theme === 'light' ? '#fff' : '#000' , color : theme === 'light' ? 'blue' : 'yellow'}}>Nabin Sharma</h1>
 }
 
 export default ContextTextComponent;
