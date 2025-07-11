1f02422f1488c0846646372f0b5f8e245202d64f
Nabin <sharmanabin.0910@gmail.com>
2025-07-11 11:57:18 +0530

    took data from API and rendered it on page

--- Sangam-course/todo/src/App.jsx
+++ Sangam-course/todo/src/App.jsx
@@ -1,11 +1,37 @@
+import {useState , useEffect } from "react";
+
 
 
 
 function App() {
 
+  const [todoList , setTodoList] = useState([]);
+  const [loading , setLoading] = useState([]);
+  const [errorMsg , setErrorMsg] = useState(null)
+
+  async function fetchListOfTodos() {
+    try {
+      setLoading(true);
+      const apiResponse = await fetch('https://dummyjson.com/todos')
+      const result = await apiResponse.json();
+
+      console.log(result)
+
+      
+
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+
+  useEffect(()=>{
+    fetchListOfTodos()
+  },[])
+
   return (
     <>
-    
+    <h1>Simple ToDo using material UI</h1>
     </>
   )
 }
