0b5f89c4003edd2528211834fad15456ae530c50
Nabin <sharmanabin.0910@gmail.com>
2025-10-16 21:48:56 +0530

    Basic imports and exports

--- Sheriyans-Coding-School/first-one/src/App.jsx
+++ Sheriyans-Coding-School/first-one/src/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <div className="card">
+        <h1>Nabin Sharma</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, voluptatem.  </p>
+      </div>
     </div>
   )
 }
