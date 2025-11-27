04ccd8810204ac95dd3af223eaf3cfa82bc5b581
Nabin <sharmanabin.0910@gmail.com>
2025-11-27 15:18:13 +0530

    Writing CSS

--- Sheriyans-Coding-School/props-drlling/src/App.jsx
+++ Sheriyans-Coding-School/props-drlling/src/App.jsx
@@ -8,17 +8,17 @@ function App() {
       <div className="card">
         <div className="top">
           <img src="https://thumbs.dreamstime.com/b/amazon-logo-white-background-montreal-canada-july-printed-paper-98221126.jpg" alt="" />
-          <button>Save <Bookmark /></button>
+          <button>Save <Bookmark size={12}/></button>
         </div>
         <div className="center">
           <h3>Amazon <span>4 days ago</span></h3>
           <h2>Senior Web Developer</h2>
-          <div>
+          <div className='tag'>
             <h4>Part Time</h4>
             <h4>Senior Level</h4>
           </div>
         </div>
-        <div className="botton">
+        <div className="bottom">
           <div>   
               <h3>$120/hr</h3>
               <p>Noida , India</p>
