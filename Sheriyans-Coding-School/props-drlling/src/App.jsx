e7391955109d83dc874fb112ea8185937ea0aad0
Nabin <sharmanabin.0910@gmail.com>
2025-11-27 13:01:43 +0530

    Basic Structure

--- Sheriyans-Coding-School/props-drlling/src/App.jsx
+++ Sheriyans-Coding-School/props-drlling/src/App.jsx
@@ -1,13 +1,31 @@
-
+import {Bookmark} from 'lucide-react'
 import './App.css'
 
 function App() {
- 
-
   return (
     <>
-     <div>
-     
+     <div className='parent'>
+      <div className="card">
+        <div className="top">
+          <img src="https://thumbs.dreamstime.com/b/amazon-logo-white-background-montreal-canada-july-printed-paper-98221126.jpg" alt="" />
+          <button>Save <Bookmark /></button>
+        </div>
+        <div className="center">
+          <h3>Amazon <span>4 days ago</span></h3>
+          <h2>Senior Web Developer</h2>
+          <div>
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        <div className="botton">
+          <div>   
+              <h3>$120/hr</h3>
+              <p>Noida , India</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
      </div>
     </>
   )
